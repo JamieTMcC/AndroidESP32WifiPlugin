220680ee3d677b0ba7a7e30f877c5cc9475d1f3f
Jamie Mccready <2459354m@student.gla.ac.uk>
2023-06-27 07:56:29 +0100

    updated plugin

--- UnityPlugin/src/main/java/com/example/unityplugin/PluginInstance.java
+++ UnityPlugin/src/main/java/com/example/unityplugin/PluginInstance.java
@@ -2,12 +2,14 @@ package com.example.unityplugin;
 
 import android.app.Activity;
 import android.widget.Toast;
-
+import java.io.*;
+import java.net.*;
 public class PluginInstance {
 
     public long getTime(){
         return System.currentTimeMillis();
     }
+
 
     private static Activity unityActivity;
 
@@ -19,4 +21,17 @@ public class PluginInstance {
         Toast.makeText(unityActivity, message, Toast.LENGTH_SHORT).show();
     }
 
+    public String getHTML() throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL("http://192.168.4.1");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(conn.getInputStream()))) {
+            for (String line; (line = reader.readLine()) != null; ) {
+                result.append(line);
+            }
+        }
+        return result.toString();
+    }
 }
